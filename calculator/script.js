19345a4241ee1f65fbfcbb496b8f9d6c79500b4d
Mihancheggg <schutow.mihail@yandex.ru>
2022-05-17 13:22:17 +0300

    uclc upd

--- calculator/script.js
+++ calculator/script.js
@@ -3,49 +3,32 @@ var buttonMinus = document.getElementById("minus");
 var buttonMultiply = document.getElementById("multiply");
 var buttonDivide = document.getElementById("divide");
 
+var input1 = document.getElementById("number1");
+var input2 = document.getElementById("number2");
 
+function operate(operator){
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+    
+    if (operator === "+"){
+        var result = number1 + number2;
+    } else if (operator === "-"){
+        var result = number1 - number2;
+    }else if (operator === "*"){
+        var result = number1 * number2;
+    }else if (operator === "/"){
+        var result = number1 / number2;
+    }
+    alert(result);
+}
 
-function onPlusClick(){
-    var input1 = document.getElementById("number1");
-    var input2 = document.getElementById("number2");
+function onOperatorClick(eventObject){
+    var clickedEl = eventObject.currentTarget;
+    var operation = clickedEl.innerHTML;
+    operate(operation);
+}
 
-    var number1 = input1.value;
-    var number2 = input2.value;
-    var sum = +number1 + +number2;
-    alert(sum);
-};
-
-function onMinusClick(){
-    var input1 = document.getElementById("number1");
-    var input2 = document.getElementById("number2");
-
-    var number1 = input1.value;
-    var number2 = input2.value;
-    var minus = +number1 - +number2;
-    alert(minus);
-};
-
-function onMultiplyClick(){
-    var input1 = document.getElementById("number1");
-    var input2 = document.getElementById("number2");
-
-    var number1 = input1.value;
-    var number2 = input2.value;
-    var product = +number1 * +number2;
-    alert(product);
-};
-
-function onDivideClick(){
-    var input1 = document.getElementById("number1");
-    var input2 = document.getElementById("number2");
-
-    var number1 = input1.value;
-    var number2 = input2.value;
-    var quotient = +number1 / +number2;
-    alert(quotient);
-};
-
-buttonPlus.addEventListener("click", onPlusClick);
-buttonMinus.addEventListener("click", onMinusClick);
-buttonMultiply.addEventListener("click", onMultiplyClick);
-buttonDivide.addEventListener("click", onDivideClick);
+buttonPlus.addEventListener("click", onOperatorClick);
+buttonMinus.addEventListener("click", onOperatorClick);
+buttonMultiply.addEventListener("click", onOperatorClick);
+buttonDivide.addEventListener("click", onOperatorClick);
